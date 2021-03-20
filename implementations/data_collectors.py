e4f59bc7b2cb9f46c464b06e8152ce51b272d67e
sami061190 <sami061190@gmail.com>
2021-03-20 22:37:20 +0100

    support cache for latested datapoint for current day

--- implementations/data_collectors.py
+++ implementations/data_collectors.py
@@ -156,28 +156,49 @@ class YahooDataCollector(DataCollector):
 
     def get_latest_data_point(self, stocks: List[str], current_time: datetime) -> Dict[str, DataPoint]:
 
+        spark_data_frame_for_stock = self.spark.createDataFrame([], self.schema)
+
         stocks_dict = {}
 
         start_time = current_time.strftime("%Y-%m-%d")
         end_time = (current_time + timedelta(days=1)).strftime("%Y-%m-%d")
 
         for stock in stocks:
-            stock_data = yf.download(stock, start=start_time, end=end_time, interval="1m")
-            if len(stock_data) < 1:
-                print(f'stock data not found on yahoo finance: {stock}')
-                continue
 
-            stock_data = stock_data.rename(columns={"Adj Close": "AdjustedClose"})
-            stock_data = stock_data.reset_index()
-            stock_data.dropna(inplace=True)
-            stock_data["Datetime"] = stock_data["Datetime"].astype(str).str[:-6].astype('datetime64[ns]')
-            stock_data["Volume"] = stock_data["Volume"].astype(float)
-            stock_data["Symbol"] = stock
-            stock_data.set_index('Datetime')
+            historical_data_path = "../datasets/historical_data/"
+            folder_path = historical_data_path + start_time + "/"
+            try:
+                Path(folder_path).mkdir(parents=True, exist_ok=True)
+            except OSError:
+                print(f'Creation of the directory {folder_path} failed')
 
-            date_filter_string = "Datetime < '" + current_time.strftime("%Y-%m-%d %H:%M:%S") + "'"
-            data_frame = self.spark.createDataFrame(stock_data, self.schema)
-            last_point_row = data_frame.where(data_frame.Datetime <= current_time.strftime("%Y-%m-%d %H:%M:%S")).sort("Datetime", ascending=False).limit(1).select("*").first()
+            stock_file = Path(folder_path + stock + ".csv")
+            if stock_file.is_file() and current_time < datetime.now() + timedelta(hours=-24):
+                # if stock data already downloaded, just load it
+                stock_data_spark_df = self.spark.read \
+                    .csv(str(stock_file), schema=self.schema, timestampFormat="yyyy-MM-dd HH:mm:ss", header=True)
+                spark_data_frame_for_stock = spark_data_frame_for_stock.union(stock_data_spark_df)
+            else:
+                stock_data = yf.download(stock, start=start_time, end=end_time, interval="1m")
+                if len(stock_data) < 1:
+                    print(f'stock data not found on yahoo finance: {stock}')
+                    continue
+
+                stock_data = stock_data.rename(columns={"Adj Close": "AdjustedClose"})
+                stock_data = stock_data.reset_index()
+                stock_data.dropna(inplace=True)
+                stock_data["Datetime"] = stock_data["Datetime"].astype(str).str[:-6].astype('datetime64[ns]')
+                stock_data["Volume"] = stock_data["Volume"].astype(float)
+                stock_data["Symbol"] = stock
+                stock_data.set_index('Datetime')
+                if current_time < datetime.now() + timedelta(hours=-24):
+                    stock_data.to_csv(path_or_buf=stock_file, index=False)
+
+                # date_filter_string = "Datetime < '" + current_time.strftime("%Y-%m-%d %H:%M:%S") + "'"
+                spark_data_frame_for_stock = spark_data_frame_for_stock.union(self.spark.createDataFrame(stock_data, self.schema))
+                # data_frame = self.spark.createDataFrame(stock_data, self.schema)
+
+            last_point_row = spark_data_frame_for_stock.where(spark_data_frame_for_stock.Datetime <= current_time.strftime("%Y-%m-%d %H:%M:%S")).sort("Datetime", ascending=False).limit(1).select("*").first()
             data_point = DataPoint(last_point_row.Open,
                                    last_point_row.Close,
                                    last_point_row.High,
@@ -192,11 +213,11 @@ class YahooDataCollector(DataCollector):
 def test():
     yahoo_data_collector = YahooDataCollector(60)
 
-    list_of_data_points = yahoo_data_collector.get_historical_data("AAPL", datetime(2021, 3, 17), 2)
-    print([f'{data_point.timestamp}| volume: {data_point.volume}, close: {data_point.close_price}' for data_point in list_of_data_points])
+    # list_of_data_points = yahoo_data_collector.get_historical_data("AAPL", datetime(2021, 3, 17), 2)
+    # print([f'{data_point.timestamp}| volume: {data_point.volume}, close: {data_point.close_price}' for data_point in list_of_data_points])
 
-    # data_point = yahoo_data_collector.get_latest_data_point(["AAPL"], datetime(2021, 3, 12, 14, 30, 21))
-    # print(f'{data_point["AAPL"].timestamp}| volume: {data_point["AAPL"].volume}, close: {data_point["AAPL"].close_price}')
+    data_point = yahoo_data_collector.get_latest_data_point(["AAPL"], datetime(2021, 3, 20, 14, 30, 21))
+    print(f'{data_point["AAPL"].timestamp}| volume: {data_point["AAPL"].volume}, close: {data_point["AAPL"].close_price}')
 
 
 if __name__ == '__main__':
